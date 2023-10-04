58d1fcf86ebdeb36a438361d1fc63e7936b50465
macwolf <romazverb18@gmsil.com>
2023-10-05 02:20:30 +0300

    added new component CalculateScreen

--- TestCalculator/App.tsx
+++ TestCalculator/App.tsx
@@ -5,44 +5,29 @@ import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
+import {CalculatorScreen} from "./src/Calculator.container";
 
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
 
 export const App = () => {
 
   return (
     <SafeAreaView>
-      <View>
-        <Text>
-          Hello guys
-        </Text>
+      <View style={styles.backgroundColor}>
+        <CalculatorScreen />
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  backgroundColor: {
+    backgroundColor: "black",
+    height: "100%",
+    width: "100%",
+    // flexDirection: "row",
+    alignItems: "flex-end"
   },
 });
 
