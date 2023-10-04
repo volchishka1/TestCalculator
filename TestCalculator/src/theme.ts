08809a2817636413cd5abe6258b12c490b8c0ea8
macwolf <romazverb18@gmsil.com>
2023-10-05 02:23:36 +0300

    added theme

---
+++ TestCalculator/src/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+    colors: {
+        orangeColor: "rgba(255, 132, 0, 1)",
+        darkGreyColor: "rgba(97, 97, 97, 1)",
+        greyColor: "rgba(194, 194, 194, 1)",
+        whiteColor: "rgba(255, 255, 255, 1)",
+    }
+}
