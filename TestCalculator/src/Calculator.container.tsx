dc0e4416e2fe2b7f7e88776c3054f98a417e35b4
macwolf <romazverb18@gmsil.com>
2023-10-05 02:21:57 +0300

    filled CalculatorContainer

---
+++ TestCalculator/src/Calculator.container.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {Alert} from 'react-native';
+import {ButtonComponent} from "./button/Button.component";
+import {Keyboard} from "./Keyboard.component";
+
+export const CalculatorScreen = () => {
+
+
+    return (
+        <Keyboard />
+    )
+}
