382de3b1decb2728a1d9578bafdc1fb3228d390f
macwolf <romazverb18@gmsil.com>
2023-10-05 02:20:51 +0300

    added buttonStyled

---
+++ TestCalculator/src/button/Button.style.ts
@@ -0,0 +1,45 @@
+import {StyleSheet, TextStyle, ViewStyle} from "react-native";
+import {theme} from "../theme";
+
+export type ButtonTextStyles = {
+    buttonTextWhite: TextStyle;
+    buttonTextDarkGrey: TextStyle;
+    buttonStyleOrange: ViewStyle;
+    buttonStyleGrey: ViewStyle;
+    buttonStyleDarkGrey: ViewStyle;
+};
+
+export const buttonStyle = StyleSheet.create<ButtonTextStyles>({
+    buttonTextWhite: {
+        fontWeight: "600",
+        textAlign: "center",
+        marginTop: 22,
+        fontSize: 30,
+        color: theme.colors.whiteColor,
+    },
+    buttonTextDarkGrey: {
+        fontWeight: "600",
+        textAlign: "center",
+        marginTop: 22,
+        fontSize: 30,
+        color: theme.colors.darkGreyColor,
+    },
+    buttonStyleOrange: {
+        width: 80,
+        height: 80,
+        backgroundColor: theme.colors.orangeColor,
+        borderRadius: 40,
+    },
+    buttonStyleGrey: {
+        width: 80,
+        height: 80,
+        backgroundColor: theme.colors.greyColor,
+        borderRadius: 40,
+    },
+    buttonStyleDarkGrey: {
+        width: 80,
+        height: 80,
+        backgroundColor: theme.colors.darkGreyColor,
+        borderRadius: 40,
+    },
+})
