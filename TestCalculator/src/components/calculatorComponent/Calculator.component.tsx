85c3d6c9aa58ad3bf92b9d4d015d9b3bd66a57ab
macwolf <romazverb18@gmsil.com>
2023-10-05 15:54:39 +0300

    moved files in another directory

---
+++ TestCalculator/src/components/calculatorComponent/Calculator.component.tsx
@@ -0,0 +1,81 @@
+import React, {FC} from "react";
+import {Text, View} from 'react-native';
+import {ButtonComponent} from "../../button/Button.component";
+import {CalculatorScreenStyled} from "./Calculator.styled";
+import {CalculatorViewProps} from "./Calculator.types";
+import {
+    AC,
+    DELETE_NUMBER,
+    DIVIDE,
+    DIVIDED,
+    DOT,
+    EQUAL,
+    MINUS,
+    MULTIPLIES,
+    MULTIPLY,
+    NUMBER_EIGHT,
+    NUMBER_FIVE,
+    NUMBER_FOUR,
+    NUMBER_NINE,
+    NUMBER_ONE, NUMBER_SEVEN,
+    NUMBER_SIX,
+    NUMBER_THREE,
+    NUMBER_TWO, NUMBER_ZERO, PERCENT, PLUS,
+    PLUS_MINUS
+} from "../../constants/globalText";
+
+export const CalculatorView: FC<CalculatorViewProps> = (props) => {
+    const {
+        firstNumber = "",
+        setFirstNumber = () => {},
+        secondNumber = "",
+        operation = "",
+        getResult = () => {},
+        handleNumberPress = () => {},
+        handleOperationPress = () => {},
+        handleClearPress = () => {},
+        firstNumberDisplay = () => {},
+    } = props;
+
+    return (
+        <View style={CalculatorScreenStyled.calculatorContainer}>
+            <View style={CalculatorScreenStyled.input}>
+                <Text style={CalculatorScreenStyled.screenSecondNumber}>
+                    {secondNumber}
+                    <Text style={CalculatorScreenStyled.operator}>{operation}</Text>
+                </Text>
+                {firstNumberDisplay()}
+            </View>
+            <View style={CalculatorScreenStyled.topContainer}>
+                <ButtonComponent buttonText={AC} isGrey onPress={handleClearPress} />
+                <ButtonComponent buttonText={PLUS_MINUS} isGrey onPress={() => handleOperationPress(PLUS_MINUS)} />
+                <ButtonComponent buttonText={PERCENT} isGrey onPress={() => handleOperationPress(PERCENT)} />
+                <ButtonComponent buttonText={DIVIDE} isOrange onPress={() => handleOperationPress(DIVIDED)} />
+            </View>
+            <View style={CalculatorScreenStyled.topContainer}>
+                <ButtonComponent buttonText={NUMBER_SEVEN} isDarkGrey onPress={() => handleNumberPress(NUMBER_SEVEN)} />
+                <ButtonComponent buttonText={NUMBER_EIGHT} isDarkGrey onPress={() => handleNumberPress(NUMBER_EIGHT)} />
+                <ButtonComponent buttonText={NUMBER_NINE} isDarkGrey onPress={() => handleNumberPress(NUMBER_NINE)} />
+                <ButtonComponent buttonText={MULTIPLY} isOrange onPress={() => handleOperationPress(MULTIPLIES)} />
+            </View>
+            <View style={CalculatorScreenStyled.topContainer}>
+                <ButtonComponent buttonText={NUMBER_FOUR} isDarkGrey onPress={() => handleNumberPress(NUMBER_FOUR)} />
+                <ButtonComponent buttonText={NUMBER_FIVE} isDarkGrey onPress={() => handleNumberPress(NUMBER_FIVE)} />
+                <ButtonComponent buttonText={NUMBER_SIX} isDarkGrey onPress={() => handleNumberPress(NUMBER_SIX)} />
+                <ButtonComponent buttonText={MINUS} isOrange onPress={() => handleOperationPress(MINUS)} />
+            </View>
+            <View style={CalculatorScreenStyled.topContainer}>
+                <ButtonComponent buttonText={NUMBER_ONE} isDarkGrey onPress={() => handleNumberPress(NUMBER_ONE)} />
+                <ButtonComponent buttonText={NUMBER_TWO} isDarkGrey onPress={() => handleNumberPress(NUMBER_TWO)} />
+                <ButtonComponent buttonText={NUMBER_THREE} isDarkGrey onPress={() => handleNumberPress(NUMBER_THREE)} />
+                <ButtonComponent buttonText={PLUS} isOrange onPress={() => handleOperationPress(PLUS)} />
+            </View>
+            <View style={CalculatorScreenStyled.topContainer}>
+                <ButtonComponent buttonText={NUMBER_ZERO} isDarkGrey onPress={() => handleNumberPress(NUMBER_ZERO)} />
+                <ButtonComponent buttonText={DOT} isDarkGrey onPress={() => handleNumberPress(DOT)} />
+                <ButtonComponent buttonText={DELETE_NUMBER} isDarkGrey onPress={() => setFirstNumber(firstNumber.slice(0, -1))} />
+                <ButtonComponent buttonText={EQUAL} isOrange  onPress={() => getResult()} />
+            </View>
+        </View>
+    )
+}
