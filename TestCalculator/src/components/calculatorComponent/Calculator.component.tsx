840027ad5c86d291bee4964176e3211950989cbe
macwolf <romazverb18@gmsil.com>
2023-10-05 22:03:07 +0300

    added new states in calculator comp

--- TestCalculator/src/components/calculatorComponent/Calculator.component.tsx
+++ TestCalculator/src/components/calculatorComponent/Calculator.component.tsx
@@ -30,9 +30,17 @@ export const CalculatorView: FC<CalculatorViewProps> = (props) => {
         setFirstNumber = () => {},
         secondNumber = "",
         operation = "",
+        isTruePlus = false,
+        isTrueMinus = false,
+        isTrueDivide = false,
+        isTrueMultiply = false,
         getResult = () => {},
         handleNumberPress = () => {},
         handleOperationPress = () => {},
+        handleOperationPressPlus = () => {},
+        handleOperationPressMinus = () => {},
+        handleOperationPressDivide = () => {},
+        handleOperationPressMultiply = () => {},
         handleClearPress = () => {},
         firstNumberDisplay = () => {},
     } = props;
@@ -50,25 +58,25 @@ export const CalculatorView: FC<CalculatorViewProps> = (props) => {
                 <ButtonComponent buttonText={AC} isGrey onPress={handleClearPress} />
                 <ButtonComponent buttonText={PLUS_MINUS} isGrey onPress={() => handleOperationPress(PLUS_MINUS)} />
                 <ButtonComponent buttonText={PERCENT} isGrey onPress={() => handleOperationPress(PERCENT)} />
-                <ButtonComponent buttonText={DIVIDE} isOrange onPress={() => handleOperationPress(DIVIDED)} />
+                <ButtonComponent buttonText={DIVIDE} isTrueDivide={isTrueDivide} isOrange onPress={() => handleOperationPressDivide(DIVIDED)} />
             </View>
             <View style={CalculatorScreenStyled.topContainer}>
                 <ButtonComponent buttonText={NUMBER_SEVEN} isDarkGrey onPress={() => handleNumberPress(NUMBER_SEVEN)} />
                 <ButtonComponent buttonText={NUMBER_EIGHT} isDarkGrey onPress={() => handleNumberPress(NUMBER_EIGHT)} />
                 <ButtonComponent buttonText={NUMBER_NINE} isDarkGrey onPress={() => handleNumberPress(NUMBER_NINE)} />
-                <ButtonComponent buttonText={MULTIPLY} isOrange onPress={() => handleOperationPress(MULTIPLIES)} />
+                <ButtonComponent buttonText={MULTIPLY} isTrueMultiply={isTrueMultiply} isOrange onPress={() => handleOperationPressMultiply(MULTIPLIES)} />
             </View>
             <View style={CalculatorScreenStyled.topContainer}>
                 <ButtonComponent buttonText={NUMBER_FOUR} isDarkGrey onPress={() => handleNumberPress(NUMBER_FOUR)} />
                 <ButtonComponent buttonText={NUMBER_FIVE} isDarkGrey onPress={() => handleNumberPress(NUMBER_FIVE)} />
                 <ButtonComponent buttonText={NUMBER_SIX} isDarkGrey onPress={() => handleNumberPress(NUMBER_SIX)} />
-                <ButtonComponent buttonText={MINUS} isOrange onPress={() => handleOperationPress(MINUS)} />
+                <ButtonComponent buttonText={MINUS} isTrueMinus={isTrueMinus} isOrange onPress={() => handleOperationPressMinus(MINUS)} />
             </View>
             <View style={CalculatorScreenStyled.topContainer}>
                 <ButtonComponent buttonText={NUMBER_ONE} isDarkGrey onPress={() => handleNumberPress(NUMBER_ONE)} />
                 <ButtonComponent buttonText={NUMBER_TWO} isDarkGrey onPress={() => handleNumberPress(NUMBER_TWO)} />
                 <ButtonComponent buttonText={NUMBER_THREE} isDarkGrey onPress={() => handleNumberPress(NUMBER_THREE)} />
-                <ButtonComponent buttonText={PLUS} isOrange onPress={() => handleOperationPress(PLUS)} />
+                <ButtonComponent buttonText={PLUS} isTruePlus={isTruePlus} isOrange onPress={() => handleOperationPressPlus(PLUS)} />
             </View>
             <View style={CalculatorScreenStyled.topContainer}>
                 <ButtonComponent buttonText={NUMBER_ZERO} isDarkGrey onPress={() => handleNumberPress(NUMBER_ZERO)} />
